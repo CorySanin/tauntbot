86e8c2d1012f351ad4ecfb9b8dfe8cc720b76e2d
Cory Sanin <corysanin@outlook.com>
2021-09-21 15:11:23 -0500

    Fix call to SlashCommands

--- index.js
+++ index.js
@@ -242,7 +242,7 @@ client.on('ready', () => {
     console.log('Taunt Bot by Cory Sanin');
     updateServerCount();
     SlashCommands(config);
-    setInterval(SlashCommands, 3600000);
+    setInterval(() => SlashCommands(config), 3600000);
     if (config.get('discordBotsToken')) {
         DBL(config.get('discordBotsToken'), client);
     }
