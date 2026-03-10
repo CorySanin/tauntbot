7664b641ca9da8d61ee172fbb23560af81911422
Cory Sanin <corysanin@outlook.com>
2026-03-10 16:42:39 -0500

    replace "main" with "exports"

---
+++ src/module.ts
@@ -0,0 +1,13 @@
+import { TauntBot } from "./tauntbot.js";
+import * as stats from "./stats.js";
+import * as types from "./types.js";
+import * as config from "./config.js";
+import * as commands from "./commands.js";
+
+export {
+    TauntBot,
+    config,
+    commands,
+    stats,
+    types
+};
